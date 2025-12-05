d6bbb747b05555022c1777762fc3ba98dc66a74a
Pierre Olivier Martel <pomartel@coderubik.com>
2025-12-05 09:41:46 -0500

    nvim

---
+++ .config/nvim/lua/plugins/snacks-animated-scrolling-off.lua
@@ -0,0 +1,15 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    scroll = {
+      enabled = false, -- Disable scrolling animations
+    },
+    explorer = {
+      filters = {
+        dotfiles = false,
+        gitignored = false,
+        hidden = true,
+      },
+    },
+  },
+}
---
+++ .config/nvim/lua/plugins/theme.lua
@@ -0,0 +1,1 @@
+/home/po/.config/omarchy/current/theme/neovim.lua
