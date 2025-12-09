bce688d91785a2a4597512049a300dc19898600e
Pierre Olivier Martel <pomartel@coderubik.com>
2025-12-09 16:25:33 -0500

    helper

--- bin/install/install-helper.sh
+++ bin/install/install-helper.sh
@@ -8,39 +8,40 @@
 # Installs a package if it's missing using omarchy-pkg-missing check.
 # If post_install_command is provided, it will be executed after installation.
 install-pkg() {
-    local pkg="$1"
-    local description="${2:-Package}"
-    local post_install="${3:-}"
+  local pkg="$1"
+  local description="${2:-Package}"
+  local post_install="${3:-}"
 
-    if omarchy-pkg-missing $pkg; then
-        echo "Installing $description..."
-        sudo pacman -S --noconfirm $pkg
+  if omarchy-pkg-missing $pkg; then
+    echo "Installing $description..."
+    sudo pacman -S --noconfirm $pkg
 
-        if [ -n "$post_install" ]; then
-            eval "$post_install"
-        fi
+    if [ -n "$post_install" ]; then
+      eval "$post_install"
     fi
+  fi
 }
 
 # install-pkg-yay <package_name> <description> [post_install_command]
 # Installs a package using yay if it's missing.
 install-pkg-yay() {
-    local pkg="$1"
-    local description="${2:-Package}"
-    local post_install="${3:-}"
+  local pkg="$1"
+  local description="${2:-Package}"
+  local post_install="${3:-}"
 
-    if omarchy-pkg-missing $pkg; then
-        echo "Installing $description..."
-        sudo yay -S --noconfirm $pkg
+  if omarchy-pkg-missing $pkg; then
+    echo "Installing $description..."
+    yay -S --noconfirm $pkg
 
-        if [ -n "$post_install" ]; then
-            eval "$post_install"
-        fi
+    if [ -n "$post_install" ]; then
+      eval "$post_install"
     fi
+  fi
 }
 
 webapp-missing() {
-    local app_name="$1"
-    local app_dir="$HOME/.local/share/applications/$app_name.desktop"
-    [ ! -e "$app_dir" ]
+  local app_name="$1"
+  local app_dir="$HOME/.local/share/applications/$app_name.desktop"
+  [ ! -e "$app_dir" ]
 }
+
