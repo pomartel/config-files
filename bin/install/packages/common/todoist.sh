7d570fc2d2e33d480e1979b1d92e186d08ea4337
Pierre Olivier Martel <pomartel@coderubik.com>
2025-12-11 15:05:58 -0500

    todoist

---
+++ bin/install/packages/common/todoist.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+source ./install-helper.sh
+if webapp-missing "Microsoft Teams"; then
+    echo "Installing Microsoft Teams webapp..."
+    omarchy-webapp-install "Microsoft Teams" https://teams.microsoft.com/v2/ https://statics.teams.cdn.office.net/evergreen-assets/icons/microsoft_teams_logo_refresh_v2025.ico
+fi
