1c8b7dc03ef5b1d7ad9b66c2e1c6b47827eddfec
Pierre Olivier Martel <pomartel@coderubik.com>
2026-01-08 13:47:50 -0500

    remove tlp

--- bin/install/configure-tlp.sh
+++ bin/install/configure-tlp.sh
@@ -1,34 +1,10 @@
-# Remove power-profiles-daemon if installed in favor of TLP
-if systemctl is-enabled power-profiles-daemon.service &>/dev/null; then
-    sudo systemctl stop power-profiles-daemon.service
-    sudo systemctl disable power-profiles-daemon.service
-    omarchy-pkg-drop power-profiles-deamon
-fi
+sudo systemctl stop tlp.service tlp-pd.service
+sudo systemctl disable tlp.service tlp-pd.service
 
-# Check if the systemd-rfkill service and socket are already masked
-if systemctl is-enabled systemd-rfkill.service &>/dev/null; then
-    echo "Masking systemd-rfkill service and socket..."
-    sudo systemctl mask systemd-rfkill.service systemd-rfkill.socket
-fi
+sudo pacman -Rns tlp tlp-pd
 
-# Check if the symbolic link exists before creating it
-if [ ! -L /usr/local/bin/powerprofilesctl ]; then
-    echo "Creating symbolic link for powerprofilesctl..."
-    sudo ln -s /usr/bin/tlpctl /usr/local/bin/powerprofilesctl
-fi
+sudo rm -rf /etc/tlp.d /etc/default/tlp
+sudo rm -f /usr/local/bin/powerprofilesctl
 
-# Set symlinks for all files in ~/.config/tlp/*.conf to /etc/tlp.d/ only if the symlink doesn't already exist
-for config_file in "$HOME/.config/tlp/"*.conf; do
-    config_filename=$(basename "$config_file")
-    symlink_path="/etc/tlp.d/$config_filename"
-    if [ ! -L "$symlink_path" ]; then
-        echo "Creating symlink for $config_filename in /etc/tlp.d/..."
-        sudo ln -s "$config_file" "$symlink_path"
-    fi
-done
-
-# Start TLP only if it is not already active
-if ! systemctl is-active tlp &>/dev/null; then
-    echo "Starting TLP..."
-    sudo tlp start
-fi
+sudo pacman -S power-profiles-daemon
+sudo systemctl enable --now power-profiles-daemon.service
--- bin/install/packages/common/tlp.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-source ./install-helper.sh
-
-omarchy-pkg-drop "power-profiles-deamon"
-
-install-pkg "tlp" "TLP Power Management" "sudo systemctl enable tlp.service"
-install-pkg "tlp-pd" "TLP Power Management - Power Profiles Daemon" "sudo systemctl enable --now tlp-pd.service"
