cefb2bf4d89b1d62fd6d2c9bc7cbf4fdccadead8
Pierre Olivier Martel <pomartel@coderubik.com>
2026-01-20 19:41:12 -0500

    voxtype

--- bin/install/packages/common/hyprwhspr.sh
+++ bin/install/packages/common/hyprwhspr.sh
@@ -3,15 +3,3 @@ omarchy-pkg-aur-add hyprwhspr
 if ! systemctl --user is-active --quiet hyprwhspr; then
     hyprwhspr setup
 fi
-
-# Voxtype dictation tool
-# omarchy-pkg-add wtype voxtype-bin
-
-# if ! systemctl --user is-active --quiet voxtype; then
-#     voxtype setup --no-post-install
-#     voxtype setup systemd
-#     voxtype setup --download --model large-v3-turbo --quiet
-#     sudo voxtype setup gpu --enable
-
-#     omarchy-restart-waybar
-# fi
