4e57b1efb9351f93872f5fb851acd26ad07a74bb
Pierre Olivier Martel <pomartel@coderubik.com>
2026-01-21 13:48:25 -0500

    fix

--- bin/install/packages/common/onedrive.sh
+++ bin/install/packages/common/onedrive.sh
@@ -1,5 +1,5 @@
 omarchy-pkg-aur-add onedrive-abraunegg
 
 if ! systemctl is-active --quiet --user onedrive; then
-    sudo systemctl enable --now onedrive
+    systemctl enable --now --user onedrive
 fi
