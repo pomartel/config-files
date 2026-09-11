d033ee445ac9d93fe4b7f13bd0accce5a7142341
Pierre Olivier Martel <pomartel@coderubik.com>
2026-09-11 13:30:12 -0400

    Start herdr in terminal on login

--- .config/hypr/autostart.lua
+++ .config/hypr/autostart.lua
@@ -7,7 +7,7 @@ o.exec_on_start("sleep 3; uwsm-app -- 1password --silent")
 o.launch_on_start("omarchy-launch-browser")
 
 hl.on("hyprland.start", function()
-  hl.exec_cmd("xdg-terminal-exec", { workspace = "2 silent" })
+  hl.exec_cmd("xdg-terminal-exec herdr", { workspace = "2 silent" })
   hl.dispatch(hl.dsp.focus({ workspace = 1 }))
 end)
 o.exec_on_start("trash-empty 30")
